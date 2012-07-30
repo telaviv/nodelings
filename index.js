b63abb2b71b1785161fb55f0649853f23964a533
Shawn Krisman <skrisman@nodelings>
2012-07-29 21:01:51 -0700

    switched to express.

--- index.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var home = require('./cmds/home.js');
-
-http.createServer(function(request, response) {
-    homeCmd = new home.HomeCmd();
-    homeCmd.route(request, response);
-}).listen(8888);
