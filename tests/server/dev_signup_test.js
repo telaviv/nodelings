50faa7819869cfadb0dfefcf2b6f64c749feb3d7
Shawn Krisman <telaviv@github>
2012-09-07 23:46:04 -0700

    refactored to use getUser instead of checkUserExists; ds is created in beforeEach.

--- tests/server/dev_signup_test.js
+++ tests/server/dev_signup_test.js
@@ -4,28 +4,30 @@ var DevSignup = require('../../logic/dev_signup').DevSignup;
 var ObjectID = require('mongodb').ObjectID;
 require('chai').Assertion.includeStack = true;
 
-
-var checkUserExists = function(db, uid, fn) {
+var getUser = function(uid, db, fn) {
     db.collection('dev_user', function(err, collection) {
 	if (err) throw err;
 	collection.findOne({_id: uid}, {_id: 1}, function (err, item) {
 	    if (err) throw err;
-	    expect(item._id.id).to.equal(uid.id);
-	    fn();
+	    fn(item);
 	})
     });
 };
 
 describe('DevSignup', function() {
+    beforeEach(function() {
+	this.ds = new DevSignup(db);
+    });
     it('should exist.', function() {
-	var ds = new DevSignup(db);
-	expect(ds).to.exist
+	expect(this.ds).to.exist
     });
     describe('#signup()', function() {
 	it('should create a user.', function(done) {
-	    var ds = new DevSignup(db);
-	    ds.signup('cheese', 'secret', function(uid) {
-		checkUserExists(db, uid, done);
+	    this.ds.signup('cheese', 'secret', function(uid) {
+		getUser(uid, db, function(userDoc) {
+		    expect(uid.id).to.equal(userDoc._id.id);
+		    done();
+		});
 	    });
 	});
     });
