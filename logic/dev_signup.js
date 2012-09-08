1e7132fe712e076801ab49e904c759dd947fa4ba
Shawn Krisman <telaviv@github>
2012-09-08 03:55:04 -0700

    password is now hashed with bcrypt.

--- logic/dev_signup.js
+++ logic/dev_signup.js
@@ -1,24 +1,70 @@
 /**
+ * Copyright 2012 Shawn Krisman
+ *
+ *  This file is part of Nodelings.
+ *
+ *  Nodelings is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Nodelings is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Nodelings.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  *  Allows creation of a "developer" user. These are people allowed to
  *  make maps.
  */
+
+var bcrypt = require('bcrypt');
+
 var DevSignup = function(db) {
     this.db = db;
 }
 
-DevSignup.prototype.signup = function(username, password, fn) {
-    this.db.collection('dev_user', function(err, collection) {
+/**
+ * Creates a user in the db.
+ *
+ * @param {string} username
+ * @param {string} password
+ * @param {function} cb callback function.
+ *
+ * @return the encrypted user id.
+ */
+DevSignup.prototype.signup = function(username, password, cb) {
+    var that = this;
+    that.db.collection('dev_user', function(err, collection) {
 	if (err) throw err;
 	collection.insert(
-	    {username: username, authentication: {password: password}},
+	    {username: username,
+	     password_hash: that._hashPassword(password)},
 	    {safe: true},
 	    function(err, docs) {
 		if (err) throw err;
 		var userDoc = docs[0];
-		fn(userDoc._id);
+		cb(userDoc._id);
 	    }
 	);
     });
 };
+
+/**
+ * Creates a cryptographically secure password hash.
+ *
+ * @param {string} password
+ *
+ * @return {string} hashed password
+ */
+DevSignup.prototype._hashPassword = function(password) {
+    return bcrypt.hashSync(password, 8);
+}
+
 
 exports.DevSignup = DevSignup;
