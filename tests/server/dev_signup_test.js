83520bec2870193f3e84a9ce01788a1814907da0
Shawn Krisman <telaviv@github>
2012-09-07 23:07:40 -0700

    moved chai assertions to expects.

--- tests/server/dev_signup_test.js
+++ tests/server/dev_signup_test.js
@@ -1,16 +1,16 @@
-var assert = require('chai').assert;
+var expect = require('chai').expect;
 var db = require('../sandbox_db').db;
 var DevSignup = require('../../logic/dev_signup').DevSignup;
 var ObjectID = require('mongodb').ObjectID;
 require('chai').Assertion.includeStack = true;
 
 
-var assertUserExists = function(db, uid, fn) {
+var checkUserExists = function(db, uid, fn) {
     db.collection('dev_user', function(err, collection) {
 	if (err) throw err;
 	collection.findOne({_id: uid}, {_id: 1}, function (err, item) {
 	    if (err) throw err;
-	    assert.strictEqual(item._id.id, uid.id);
+	    expect(item._id.id).to.equal(uid.id);
 	    fn();
 	})
     });
@@ -19,13 +19,13 @@ var assertUserExists = function(db, uid, fn) {
 describe('DevSignup', function() {
     it('should exist.', function() {
 	var ds = new DevSignup(db);
-	assert.isDefined(ds);
+	expect(ds).to.exist
     });
     describe('#signup()', function() {
 	it('should create a user.', function(done) {
 	    var ds = new DevSignup(db);
 	    ds.signup('cheese', 'secret', function(uid) {
-		assertUserExists(db, uid, done);
+		checkUserExists(db, uid, done);
 	    });
 	});
     });
