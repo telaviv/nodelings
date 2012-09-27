e151fcf81202e9370c84da10971da1cd4d5c6474
Shawn Krisman <telaviv@github>
2012-09-27 01:43:50 -0700

    added sandbox.

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ var configuration = {
     mongo_name: 'nodelings',
     mongo_port: 27017,
     mongo_host: 'localhost',
+    mongo_sandbox: process.env.SANDBOX || false
 }
 
 exports.config = configuration;
